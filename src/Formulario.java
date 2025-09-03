a63356fbbcf9fdd894778d717beeff74c9ba57b1
guillermo.uscudum <guillermo.uscudum@utec.edu.uy>
2025-09-03 11:22:07 -0300

    Formulario de registro

---
+++ src/Formulario.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Formulario extends JFrame {
+    private JLabel lblTitulo;
+    private JButton btnConfirmar;
+    private JPanel contentPane;
+
+
+    public static void main(String[] args) {
+        Formulario frame = new Formulario();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setSize(400,400);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setContentPane(frame.contentPane);
+    }
+}
